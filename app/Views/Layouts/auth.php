4085227c3240c3f4b61a7d5647d6fa98e8ed46d6
jayant bhunia <bhuniajayant@gmail.com>
2022-12-24 01:08:22 +0530

    login page

---
+++ app/Views/Layouts/auth.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>login Page</title>
+	<!-- Font Awesome -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+  rel="stylesheet"
+/>
+<!-- Google Fonts -->
+<link
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+  rel="stylesheet"
+/>
+<!-- MDB -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.0.1/mdb.min.css"
+  rel="stylesheet"
+/>
+<!-- page css -->
+<?= $this->renderSection('css') ?>
+</head>
+<body>
+  
+<!-- =================================================== -->
+<!-- content  -->
+<!-- =================================================== -->
+<?= $this->renderSection('content') ?>
+<!-- =================================================== -->
+<!-- end content -->
+<!-- =================================================== -->
+
+<script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.0.1/mdb.min.js"
+></script>
+</body>
+</html>
