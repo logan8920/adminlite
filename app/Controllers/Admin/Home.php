048bb4a24e0a900906e0000be826edade1211240
unknown <bhuniajayant@gmail.com>
2022-12-25 09:30:27 +0530

    admin changes

---
+++ app/Controllers/Admin/Home.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Controllers\BaseController;
+
+class Home extends BaseController
+{
+    public $_accountsObj;
+    public $_session;
+
+    //load site helper session
+    public function __construct(){
+
+        helper(['site','uri','form']);
+        //$this->_accountsObj  = new AccountsModel();
+        $this->_session = session();
+        check_admin_logout();
+    }
+
+    //load dashboard
+    public function index()
+    {
+        $data['page_title'] = 'Admin | Dashboard';
+        return view('Admin/dashboard',$data);
+    }
+
+    //demo 
+    public function add_product()
+    {
+        return view('admin/demo');
+    }
+}
